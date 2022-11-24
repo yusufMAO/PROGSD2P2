085952ee5d32754cac340ca9b5e6ebdae5563d5b
yusufMAO <90691334+yusufMAO@users.noreply.github.com>
2022-11-24 12:58:26 +0100

    week1opdracht2

---
+++ week1/PROGCMS-week1/plubic/index.php
@@ -0,0 +1,9 @@
+<?php
+
+include "./week1/PROGCMS-week1/private/voorbeeldinclude.php";
+echo "Included";
+
+include "./week1/PROGCMS-week1/private/voorbeeldrequired.php";
+echo "Required";
+
+
---
+++ week1/PROGCMS-week1/startpagina.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Startpagina</title>
+</head>
+<body>
+<header>
+  Welkom
+</header>
+
+<h1>
+  Standaard tekst
+</h1>
+
+<footer>
+
+</footer>
+  Privacy verklaring
+</body>
+</html>
