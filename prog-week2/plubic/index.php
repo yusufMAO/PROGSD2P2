cecb286196ef7d07999384df5642b4ab7e23baaa
yusufMAO <90691334+yusufMAO@users.noreply.github.com>
2022-12-01 13:06:15 +0100

    laatste opdracht gaan volgende week maken metsimon

---
+++ prog-week2/plubic/index.php
@@ -0,0 +1,14 @@
+<?php
+
+include "../private/shared/include.php";
+echo "Included";
+
+include "../private/shared/voorbeeldrequired.php";
+echo "Required";
+
+
+include "../private/shared/footer.php";
+echo "Footer";
+
+include "../private/shared/header.php";
+echo "Header";
---
+++ prog-week2/startpagina.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Startpagina</title>
+    <link rel="stylesheet" href="../PROGCMS-week1/css/style.css">
+</head>
+<body>
+
+<?php include "./private/shared/header.php";?>
+
+<h1>
+  Startpagina
+</h1>
+
+<?php include "./private/shared/footer.php";?>
+
+</body>
+</html>
