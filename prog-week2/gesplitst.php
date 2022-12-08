40169a6443f7972aec52fad30dfe3c172cb0ee64
yusufMAO <90691334+yusufMAO@users.noreply.github.com>
2022-12-08 12:40:17 +0100

    opdtracht

---
+++ prog-week2/gesplitst.php
@@ -0,0 +1,7 @@
+<?php include "../private/shared/header.php"; ?>
+
+<h1>
+  Standaard tekst
+</h1>
+
+<?php include "../private/shared/footer.php"; ?>
---
+++ prog-week2/private/shared/form.php
@@ -0,0 +1,54 @@
+<?php
+$email = "mediacc4olge.c#om";
+
+$email = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo("$email is a valid email adress");
+} else {
+    echo("$email is not a valid email address");
+}
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form.php</title>
+    <link rel="stylesheet" href="../../css/style.css">
+
+</head>
+<body>
+    <form action="" method="post">
+        <p>
+            <label for="naam">Name:</label>
+            <input type="text" name="name" id="naam" />
+        </p>
+        <br>
+        <br>
+        <p>
+            <label for="e-mail">E-mail:</label>
+            <input type="text" name="e-mail" id="e-mail" />
+        </p>
+        <br>
+        <br>
+        <p>
+            <label for="commentaar">Vraag?</label>
+            <input type="text" name="commentaar" id="commentaar" />
+        </p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p>
+            <input type="submit" value="Verzenden" />
+        </p>
+    </form>
+</body>
+</html>
