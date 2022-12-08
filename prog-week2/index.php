f3cad760ee1fc52c5e193fc86a14e3d7e736329f
yusufMAO <90691334+yusufMAO@users.noreply.github.com>
2022-12-08 12:28:59 +0100

    opdracht

---
+++ prog-week2/index.php
@@ -0,0 +1,18 @@
+
+
+<?php
+
+include  "../prog-week2/private/init.php";
+
+
+
+// include SHARED_PATH . "/header.php";
+
+
+include  "../prog-week2/public/startpagina.php";
+
+include SHARED_PATH . "/footer.php";
+
+
+
+?>  
---
+++ prog-week2/public/startpagina.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Startpagina</title>
+  <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+
+  <header>
+    <?php include "../prog-week2/private/shared/header.php"; ?>
+  </header>
+
+  <h1>
+    Startpagina
+  </h1>
+
+  <?php include "../prog-week2/private/shared/footer.php"; ?>
+
+</body>
+
+</html>
