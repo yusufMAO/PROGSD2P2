bd37e08dc64e0e53422ae1f02737374427284e70
yusufMAO <90691334+yusufMAO@users.noreply.github.com>
2022-12-14 11:45:04 +0100

    week3-1

---
+++ prog-week2/include.php
@@ -0,0 +1,3 @@
+<?php
+include "voorbeeld.php";
+?>
---
+++ prog-week2/private/shared/init.php
@@ -0,0 +1,54 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>init</title>
+    <link rel="stylesheet" href="../../css/init.css">
+</head>
+
+<body>
+    <div class="init">
+<?php
+
+
+define("PRIVATE_PATH", dirname(__FILE__));
+define("PROJECT_PATH", dirname(PRIVATE_PATH));
+
+
+define("SHARED_PATH", PRIVATE_PATH . '/shared');   
+define("INIT_PATH", PRIVATE_PATH . '/shared');   
+
+//echo $_SERVER['SCRIPT_NAME'];
+//echo $_SERVER['DOCUMENT_ROOT'];
+
+//echo PROJECT_PATH;
+//echo PRIVATE_PATH;
+//echo SHARED_PATH;
+//echo __FILE__;
+
+
+$root = substr($_SERVER['SCRIPT_NAME'],
+    0,
+    strpos($_SERVER['SCRIPT_NAME'], '/public') + 7);
+define("ROOT_URL", $root);
+
+function url_path($path) {
+    if($path[0] != '/') {
+        $path = "/" . $path;
+    }
+    return ROOT_URL . $path;
+}
+
+
+
+
+?> 
+</div>
+
+</body>
+
+</html>
